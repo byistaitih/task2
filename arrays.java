059032100b628fc5469455348b006465982e97e9
basmala <b.y.istaitih@students.ptuk.edu.ps>
2025-04-15 21:48:31 +0300

    creative basmala

--- arrays.java
+++ arrays.java
@@ -1,8 +1,9 @@
 
 public class arrays{
     public static void main(String [] args){
-    System.out.println("it`s work");
-    
+    System.out.print("it`s work");
+    System.out.println("gi");
+    System.out.println("creative basmala ");
 
 
     }
